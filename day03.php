21513c3f55253a39cfc2f10f139e3b213b660da8
Kalecser Kurtz <kalecser@ebanx.com>
2025-12-04 08:28:56 -0300

    day-03 wip

--- day03.php
+++ day03.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Part 1 sample expected: 357 actual: " . compute(sample()) . "\n";
-echo "Part 1 actual expected: 17155 actual: " . compute(input()) . "\n";
+//echo "Part 1 sample expected: 357 actual: " . compute(sample()) . "\n";
+//echo "Part 1 actual expected: 17155 actual: " . compute(input()) . "\n";
 echo "Part 2 sample expected: xxx actual:" . compute2(sample()) . "\n";
 //echo "Part 2 actual expected: xxx actual: " . compute(input()) . "\n";
 
@@ -36,11 +36,27 @@ function compute2(string $input): string {
     $banks = explode("\n", $input);
 
     $jolts = 0;
-
     foreach ($banks as $bank) {
         $batteries = str_split($bank);
 
+        $digits = 12;
+        $pack = '';
 
+        while ($digits > 0) {
+            $remaining_banks = array_slice($batteries, 0, ($digits - 1) * -1);
+            if (empty($remaining_banks)) {
+                $pack .= $batteries[0];
+                break;
+            }
+            $max = max($remaining_banks);
+            $index = array_search($max, $remaining_banks);
+            $pack .= $remaining_banks[$index];
+            $batteries = array_slice($batteries, $index + 1);
+            $digits -= 1;
+        }
+
+        var_dump($pack);
+        $jolts += (int)$pack;
     }
 
     return $jolts;
