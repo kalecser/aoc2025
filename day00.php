d8086f17524f3ad313b15f478e8e6e0da01950f2
kalecser <kalecser@ebanx.com>
2025-12-11 13:59:29 -0300

    update sample

--- day00.php
+++ day00.php
@@ -1,18 +1,18 @@
 <?php
 
 echo "Part 1 sample expected: xxx actual: " . compute(sample()) . "\n";
-echo "Part 1 actual expected: xxx actual: " . compute(input()) . "\n";
-echo "Part 2 sample expected: xxx actual:" . compute(sample()) . "\n";
-echo "Part 2 actual expected: xxx actual: " . compute(input()) . "\n";
+//echo "Part 1 actual expected: xxx actual: " . compute(input()) . "\n";
+//echo "Part 2 sample expected: xxx actual:" . compute(sample()) . "\n";
+//echo "Part 2 actual expected: xxx actual: " . compute(input()) . "\n";
 
 function compute(string $input): string {
     return 'foobar';
 }
 
-function sample() {
+function sample(): string {
     return file_get_contents(pathinfo(__FILE__, PATHINFO_FILENAME) . '-0sample.txt');
 }
 
-function input() {
+function input(): string {
     return file_get_contents(pathinfo(__FILE__, PATHINFO_FILENAME) . '-input.txt');
 }
